62d5ea1e6c9d8a46f4b09c6d593966aa0f14bc78
Jonathan Miller <jon@jmill.dev>
2023-02-20 10:00:11 -0800

    Change fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import * as Fathom from "fathom-client";
+import { useFathom } from "@/hooks/useFathom";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (process.env.NODE_ENV !== "production") return;
-    Fathom.load(process.env.NEXT_PUBLIC_FATHOM_CODE as string, {
-      includedDomains: ["spaghettify.dev"],
-    });
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview();
-    }
-    // Record a pageview when route changes
-    router.events.on("routeChangeComplete", onRouteChangeComplete);
-
-    // Unassign event listener
-    return () => {
-      router.events.off("routeChangeComplete", onRouteChangeComplete);
-    };
-  }, []);
+  useFathom("MZGKSHWL", {
+    url: "https://ruby-parrot.spaghettify.dev/script.js",
+    includedDomains: ["spaghettify.dev"],
+    spa: "auto",
+  });
 
   return <Component {...pageProps} />;
 }
