c689142a953146b012e32d16800804e329701258
Jonathan Miller <jon@jmill.dev>
2023-02-20 09:17:33 -0800

    Add prod check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
+    if (process.env.NODE_ENV !== "production") return;
     Fathom.load(process.env.NEXT_PUBLIC_FATHOM_CODE as string, {
       includedDomains: ["spaghettify.dev"],
     });
